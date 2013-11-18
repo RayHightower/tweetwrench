8ca713eadedd23fcfc3f371b7327eb8f45aed5fe
Raymond T. Hightower <rth@wisdomgroup.com>
2013-11-18 00:02:58 -0600

    examine the JSONKit header file, and determine what methods are available

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -21,8 +21,42 @@ class TweetViewController < UITableViewController
   def viewWillAppear(animated)
     super
     self.view.backgroundColor = UIColor.whiteColor
+    @feed = []
+    timer_fired(self)
+    @timer = NSTimer.scheduledTimerWithTimeInterval(2, 
+          target:self, 
+          selector:'timer_fired:', 
+          userInfo:nil, repeats:true)
   end
   
+  def viewWillDisappear(animated)
+    @timer.valid = false
+    @timer = nil
+  end
+
+  def timer_fired(sender)
+    @twitter_accounts = %w(dhh google)
+    query = @twitter_accounts.map{ |account| "from:#{account}" }.join(" OR ")
+    url_string = "http://search.twitter.com/search.json?q=#{query}"
+    
+    error_ptr = Pointer.new(:object)
+    BW::HTTP.get(url_string) do |response|
+      parsed = response.body.to_str.objectFromJSONStringWithParseOptions(
+        JKParseOptionValidFlags, 
+        error: error_ptr
+        )
+      if parsed.nil?
+        error = error_ptr[0]
+        puts error.userInfo[NSLocalizedDescriptionKey]
+        @timer.valid = false
+      else
+        parsed.each do |item|
+          puts item.inspect
+        end
+      end
+    end
+  end
+
   def tableView(tableView, numberOfSectionsInTableView:section)
     1
   end
